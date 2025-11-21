f376fdb295a1edc05e19ccd4a4b55ae7914c86cb
Berzinsk <bissotto.bernardo@gmail.com>
2025-11-21 11:19:10 -0300

    Criação da função logout e estilizado as interfaces

--- app/src/main/java/com/example/trabalhoiii/activity/MainActivity.kt
+++ app/src/main/java/com/example/trabalhoiii/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.example.trabalhoiii.R
 
 class MainActivity : AppCompatActivity() {
@@ -11,22 +12,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonGoToAluno = findViewById<Button>(R.id.buttonGoToAluno)
-        buttonGoToAluno.setOnClickListener {
+        // Encontra os CardViews
+        val cardAluno = findViewById<CardView>(R.id.cardAluno)
+        val cardTreino = findViewById<CardView>(R.id.cardTreino)
+        val cardExercicio = findViewById<CardView>(R.id.cardExercicio)
+        val buttonLogout = findViewById<Button>(R.id.buttonLogout)
+
+        // Configura os cliques
+        cardAluno.setOnClickListener {
             val intent = Intent(this, AlunoActivity::class.java)
             startActivity(intent)
         }
 
-        val buttonGoToTreino = findViewById<Button>(R.id.buttonGoToTreino)
-        buttonGoToTreino.setOnClickListener {
+        cardTreino.setOnClickListener {
             val intent = Intent(this, TreinoActivity::class.java)
             startActivity(intent)
         }
 
-        val buttonGoToExercicio = findViewById<Button>(R.id.buttonGoToExercicio)
-        buttonGoToExercicio.setOnClickListener {
+        cardExercicio.setOnClickListener {
             val intent = Intent(this, ExercicioActivity::class.java)
             startActivity(intent)
+        }
+
+        buttonLogout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
         }
     }
 }
